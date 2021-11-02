2ea3b43d8174aa5475799a242b018d79ddbd3029
Jonny <j.p.sharrock1@gmail.com>
2021-11-02 12:06:25 +0000

    Fixed issue for netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Homepage from './pages/Homepage'
+import Login from './pages/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     {/* Navbar here */}
       <Switch>
         <Route exact path="/home" component={Homepage}/>
+        <Route exact path="/login" component={Login}/>
       </Switch>
     </Router>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,10 @@
 import './Item.css'
-import snakeplant from '../assets/snake-plant.jpeg'
 
 const Item = () => {
     return (
         <div className="card">
             <div className="image">
-                {/* <img src={snakeplant} alt="" /> */}
+                {}
             </div>
             <div className="info">
 
