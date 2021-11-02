85cbe2689e7c5eea4ea24668d583e0ceaa131c2a
Alex Saxton <alex.saxton98@gmail.com>
2021-11-02 16:09:40 +0000

    Testing connection locally

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,36 @@
 import './Homepage.css'
+import { useState } from 'react';
 import Featured from '../components/Featured'
 
+
+
+
+
 const Homepage = () => {
+    
+const [data, setData] = useState()
+
+const handleFetch = async () => {
+    console.log("got here 1")
+    try {
+        console.log("got here 2")
+        let response = await fetch("http://localhost:80/product")
+        let data = await response.json()
+        let info = JSON.stringify(data)
+        setData(info)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+let pageInfo = data
+
     return (
         <div className="home">
             <div className="content">
                 <Featured />
+                <button onClick={handleFetch}>Hello</button>
+                <p>{pageInfo}</p>
             </div>
         </div>
     )
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,36 +1,36 @@
-import { useState } from 'react'
-import Axios from 'axios'
-import './Login.css'
+// import { useState } from 'react'
+// import Axios from 'axios'
+// import './Login.css'
 
-const Login = () => {
+// const Login = () => {
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+//     const [username, setUsername] = useState("")
+//     const [password, setPassword] = useState("")
 
-    Axios.defaults.withCredentials = true
+//     Axios.defaults.withCredentials = true
 
-    const login = () => {
-    Axios.post('http://localhost/users/login', {
-      username: username,
-      password: password
-    }).then((response) => {
-            console.log(response)
-        })
-  }
+//     const login = () => {
+//     Axios.post('http://localhost/users/login', {
+//       username: username,
+//       password: password
+//     }).then((response) => {
+//             console.log(response)
+//         })
+//   }
 
-    return (
-        <div className="login">
-            <div className="content">
-                <p>Logo Goes Here</p>
-                <form>
-                    <input type="text" placeholder="Email..." onChange={e => setUsername(e.target.value)}/>
-                    <input type="password" placeholder="Password..." onChange={e => setPassword(e.target.value)}/>
-                    <input type="submit" value="Login" onClick={login}/>
-                </form>
-                <p>Don't have an account? <span className="signup">Sign up.</span> </p>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className="login">
+//             <div className="content">
+//                 <p>Logo Goes Here</p>
+//                 <form>
+//                     <input type="text" placeholder="Email..." onChange={e => setUsername(e.target.value)}/>
+//                     <input type="password" placeholder="Password..." onChange={e => setPassword(e.target.value)}/>
+//                     <input type="submit" value="Login" onClick={login}/>
+//                 </form>
+//                 <p>Don't have an account? <span className="signup">Sign up.</span> </p>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Login
+// export default Login
