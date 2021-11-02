2e2dd80ea94902eed0a0c01dbafcb2df0c99ce63
Becky Fox <foxy2530@gmail.com>
2021-11-02 11:36:55 +0000

    first working navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/pages/Home";
+import { Test } from "./components/pages/Test";
+import { Test2 } from "./components/pages/Test2";
+import { Login } from "./components/pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Test" component={Test} />
+            <Route path="/Test2" component={Test2} />
+            <Route path="/Login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
