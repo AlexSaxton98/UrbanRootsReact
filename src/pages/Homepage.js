b2413a89e4342d937cb4937b545639c826759498
Alex Saxton <alex.saxton98@gmail.com>
2021-11-03 14:00:13 +0000

    Progress on integrating backend data

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,36 @@
 import './Homepage.css'
 import Featured from '../components/Featured'
-
+import React, { useState, useEffect } from 'react';
 import Product from "../components/product"
 
 const Homepage = () => {
+
+    const [data, setData] = useState([{}])
+
+    useEffect(() => {
+        handleFetch()
+    }, [])
+
+const handleFetch = async () => {
+    console.log("got here 1")
+    try {
+        console.log("got here 2")
+        let response = await fetch("http://localhost:80/product")
+        let data = await response.json()
+        console.log(data)
+        setData(data[0])
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
     return (
         <div className="home">
-            <div className="content_homepage">
+            <div className="content">
                 <Featured />
+                <img src={data.imageUrl} />
+                <p>{data}</p>
                 <Product />
             </div>
         </div>
