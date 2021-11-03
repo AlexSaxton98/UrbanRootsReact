9ac979e22dbd001d5f320424cc79b92668828e51
Alex Saxton <alex.saxton98@gmail.com>
2021-11-03 16:40:54 +0000

    Fixed Extra Products

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,10 +2,7 @@ import './Homepage.css'
 import Featured from '../components/Featured'
 import React, { useState, useEffect } from 'react';
 import Product1 from "../components/product"
-import Product2 from "../components/product1"
-import Product3 from "../components/product2"
-import Product4 from "../components/product3"
-import Product5 from "../components/product4"
+
 
 const Homepage = () => {
 
@@ -15,10 +12,6 @@ const Homepage = () => {
                 <Featured />
                 
                 <Product1 />
-                <Product2 />
-                {/* <Product3 />
-                <Product4 />
-                <Product5 /> */}
                 
             </div>
         </div>
