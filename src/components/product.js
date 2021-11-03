1b32afe30dc5a9198b61bc0aeb6597f7198f1a84
Alex Saxton <alex.saxton98@gmail.com>
2021-11-03 16:32:36 +0000

    Added Info modals, connecting to local backend database

--- src/components/product.js
+++ src/components/product.js
@@ -1,8 +1,29 @@
 import Modal from 'react-modal';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./product.css";
 
 const Product = () =>{
+
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+      handleFetch()
+  }, [])
+
+const handleFetch = async () => {
+  console.log("got here 1")
+  try {
+      console.log("got here 2")
+      let response = await fetch("http://localhost:80/product")
+      let info = await response.json()
+      console.log(info.data)
+      setData(info.data[0])
+  } catch (error) {
+      console.log(error)
+  }
+}
+
   const [showModal, setShowModal] = useState(false);
 
   const handleShowModal = () => {
@@ -14,7 +35,8 @@ const Product = () =>{
   };
   return (
     <div>
-      <button onClick={handleShowModal}>Product</button>
+      {/* <button >Product</button> */}
+      <img className="prodImg" src={data.imageUrl} onClick={handleShowModal}/>
        <Modal 
                     isOpen={showModal}
                     ariaHideApp={false}
@@ -26,12 +48,14 @@ const Product = () =>{
                     }}
                     >
                       <div className="modalContainer">
+                          
+                          <img className="prodImg" src={data.imageUrl} />
+                          <div className="prodInfo">
+                            <p>{data.name}</p>
+                            <p>{data.description}</p>
+                            </div>
                           <div className="xDiv">
                               <button onClick={handleHideModal} className="x">×</button>
-                          </div>
-                          <div>
-                              
-                              
                           </div>
                        </div>
                 </Modal>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,37 +1,25 @@
 import './Homepage.css'
 import Featured from '../components/Featured'
 import React, { useState, useEffect } from 'react';
-import Product from "../components/product"
+import Product1 from "../components/product"
+import Product2 from "../components/product1"
+import Product3 from "../components/product2"
+import Product4 from "../components/product3"
+import Product5 from "../components/product4"
 
 const Homepage = () => {
-
-    const [data, setData] = useState([{}])
-
-    useEffect(() => {
-        handleFetch()
-    }, [])
-
-const handleFetch = async () => {
-    console.log("got here 1")
-    try {
-        console.log("got here 2")
-        let response = await fetch("http://localhost:80/product")
-        let info = await response.json()
-        console.log(info.data)
-        setData(info.data[0])
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 
     return (
         <div className="home">
             <div className="content">
                 <Featured />
-                <img src={data} />
-                <p>{data}</p>
-                <Product />
+                
+                <Product1 />
+                <Product2 />
+                {/* <Product3 />
+                <Product4 />
+                <Product5 /> */}
+                
             </div>
         </div>
     )
