9c67a5d4bfa9b6b7e4cc6de38d6e26a9944621c1
Alex Saxton <alex.saxton98@gmail.com>
2021-11-05 15:42:18 +0000

    updated homepage and products

--- src/components/product1.js
+++ src/components/product1.js
@@ -1,11 +1,11 @@
 import Modal from 'react-modal';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import "./product.css";
 import axios from 'axios'
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const Product = ({id}) =>{
+const Product = ({ name, price, category, imageUrl, description }) =>{
   const [showModal, setShowModal] = useState(false);
     const handleShowModal = () => {
         setShowModal(true);
@@ -14,30 +14,13 @@ const Product = ({id}) =>{
         setShowModal(false);
     };
 
-  const [data, setData] = useState([{}])
-
-    useEffect(() => {
-      const handleFetch = async () => {
-    try {
-        console.log(BASE_URL)
-        let response = await fetch(`${BASE_URL}/product`)
-        let info = await response.json()
-        console.log(info.data)
-        setData(info.data[id])
-    } catch (error) {
-        console.log(error)
-    }
-  }
-            handleFetch()
-        }, [id])
-
 
 const handleClick = async () => {
   axios.post(`${BASE_URL}/basket/`, {
-    productName: data.productName,
-    price: data.price,
-    category: data.category,
-    imageUrl: data.imageUrl
+    productName: name,
+    price: price,
+    category: category,
+    imageUrl: imageUrl
   }).then((response) => {
     console.log(response)
   })
@@ -51,9 +34,9 @@ const handleClick = async () => {
   return (
     <div>
       {/* <button >Product</button> */}
-      {data.imageUrl ? 
+      {imageUrl ? 
 <>
-      <img className="prodImgMain" src={data.imageUrl} onClick={handleShowModal} alt="plant"/>
+      <img className="prodImgMain" src={imageUrl} onClick={handleShowModal} alt="plant"/>
        <Modal 
                     isOpen={showModal}
                     ariaHideApp={false}
@@ -66,12 +49,12 @@ const handleClick = async () => {
                     >
                       <div className="modalContainer">
                           
-                          <img className="prodImg" src={data.imageUrl} alt="plant"/>
+                          <img className="prodImg" src={imageUrl} alt="plant"/>
                           <div className="prodInfo">
-                            <p>{data.productName}</p>
-                            <p>{data.description}</p>
-                            <p>£{data.price}</p>
-                            <button onClick={() => handleClick(data)}>Add To Basket</button>
+                            <p>{name}</p>
+                            <p>{description}</p>
+                            <p>£{price}</p>
+                            <button onClick={() => handleClick()}>Add To Basket</button>
                             </div>
                           <div className="xDiv">
                               <button onClick={handleHideModal} className="x">×</button>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,26 +1,41 @@
-import './Homepage.css'
+import "./Homepage.css";
+import { useState, useEffect } from "react";
+import Featured from "../components/Featured";
+import Product from "../components/product1";
+import axios from "axios";
 
-import Featured from '../components/Featured'
-import Product from "../components/product1"
-
-
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const Homepage = () => {
-    
+  const [products, setProducts] = useState([]);
 
-    return (
-        <div className="home">
-            <div className="content">
-                <Featured />
-                
-                {[...Array(1).keys()].map(num => <Product id={num} />)}
-                
-               
-                
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    const handleFetch = async () => {
+      try {
+        axios.get(`${BASE_URL}/product`).then(response => {
+            console.log(response.data)
+            setProducts(response.data.data)
+        }) 
 
+        
+        // const info = await response.json();
+        // console.log(info.data.products);
+        // setProducts(info.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleFetch();
+  }, []);
 
-export default Homepage
+  return (
+    <div className="home">
+      <div className="content">
+        <Featured />
+        {products.map(product => <Product key={product.id} name={product.name} price={product.price} category={product.category} imageUrl={product.imageUrl} description = {product.description} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
